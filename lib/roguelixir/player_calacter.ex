1a1494f0a1a9532c3aef7eea69ab315d135a4003
hayabusa333 <junnichi0121@gmail.com>
2020-10-19 20:23:26 +0900

    プレイヤーがどこのダンジョンにいるかを持たせられるようにする

--- lib/roguelixir/player_calacter.ex
+++ lib/roguelixir/player_calacter.ex
@@ -1,10 +1,14 @@
 defmodule Roguelixir.PlayerCalacter do
-  alias __MODULE__
+  alias Roguelixir.{PlayerCalacter, DungeonMap}
 
   @enforce_keys [:row, :col]
   defstruct [:row, :col, :place]
 
   def new do
     {:ok, %PlayerCalacter{row: 0, col: 0}}
+  end
+
+  def dive_dungeon(player, dungeon) do
+    {:ok, %PlayerCalacter{player | place: dungeon}}
   end
 end
