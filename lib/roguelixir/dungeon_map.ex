889ef6f2ddbba2adc9f039f9abca1dc2089bacf4
hayabusa333 <junnichi0121@gmail.com>
2020-10-23 21:29:52 +0900

    add coordinate

--- lib/roguelixir/dungeon_map.ex
+++ lib/roguelixir/dungeon_map.ex
@@ -2,9 +2,9 @@ defmodule Roguelixir.DungeonMap do
   alias Roguelixir.DungeonMap
 
   @enforce_keys [:row, :col, :dungeon]
-  defstruct [:row, :col, :dungeon]
+  defstruct [:row, :col, :dungeon, :coordinate]
 
   def new(dungeon) do
-    {:ok, %DungeonMap{row: 100, col: 100, dungeon: dungeon}}
+    {:ok, %DungeonMap{row: 100, col: 100, dungeon: dungeon, coordinate: {}}}
   end
 end
