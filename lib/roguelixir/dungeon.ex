7f611bf136a5d9b859594a68fc96b71c6b9d2dc3
hayabusa333 <junnichi0121@gmail.com>
2020-10-26 20:35:10 +0900

    dungeonに基本的な大きさを持てるようにする

--- lib/roguelixir/dungeon.ex
+++ lib/roguelixir/dungeon.ex
@@ -1,10 +1,10 @@
 defmodule Roguelixir.Dungeon do
   alias __MODULE__
 
-  defstruct [:name, :stratum, :text]
+  defstruct [:name, :stratum, :text, :row, :col]
 
   def new(name, stratum) do
-    dungeon = %Dungeon{name: name, stratum: stratum}
+    dungeon = %Dungeon{name: name, stratum: stratum, row: 100, col: 100}
     :ets.new(:roguelixir_dungeon, [:set, :protected, :named_table])
     :ets.insert(:roguelixir_dungeon, {:dungeon, dungeon})
     {:ok}
