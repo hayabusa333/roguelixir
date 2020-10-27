94dc08ca8f765d9cbee39f66ecf5afdbd8ec4fc4
hayabusa333 <junnichi0121@gmail.com>
2020-10-27 22:06:58 +0900

    ymlからデータを読み取るようにする

--- lib/roguelixir/dungeon.ex
+++ lib/roguelixir/dungeon.ex
@@ -3,8 +3,10 @@ defmodule Roguelixir.Dungeon do
 
   defstruct [:name, :stratum, :text, :row, :col]
 
-  def new(name, stratum) do
-    dungeon = %Dungeon{name: name, stratum: stratum, row: 100, col: 100}
+  def new() do
+    path = Path.join(File.cwd!(), "config/yml/dungeon.yml")
+    {:ok, yml_data} = YamlElixir.read_from_file(path)
+    dungeon = %Dungeon{name: yml_data["dungeon1"]["name"], stratum: yml_data["dungeon1"]["stratum"], row: 100, col: 100}
     :ets.new(:roguelixir_dungeon, [:set, :protected, :named_table])
     :ets.insert(:roguelixir_dungeon, {:dungeon, dungeon})
     {:ok}
