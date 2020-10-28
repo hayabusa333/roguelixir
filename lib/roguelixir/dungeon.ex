8893b5149f0e8b515c2dd9f0e6d88ae73388e82b
hayabusa333 <junnichi0121@gmail.com>
2020-10-28 21:58:43 +0900

    yamlの呼び出しを外だし

--- lib/roguelixir/dungeon.ex
+++ lib/roguelixir/dungeon.ex
@@ -4,8 +4,7 @@ defmodule Roguelixir.Dungeon do
   defstruct [:name, :stratum, :text, :row, :col]
 
   def new() do
-    path = Path.join(File.cwd!(), "config/yml/dungeon.yml")
-    {:ok, yml_data} = YamlElixir.read_from_file(path)
+    yml_data = load_yaml()
     dungeon = %Dungeon{name: yml_data["dungeon1"]["name"], stratum: yml_data["dungeon1"]["stratum"], row: 100, col: 100}
     :ets.new(:roguelixir_dungeon, [:set, :protected, :named_table])
     :ets.insert(:roguelixir_dungeon, {:dungeon, dungeon})
@@ -14,5 +13,11 @@ defmodule Roguelixir.Dungeon do
 
   def self do
     :ets.lookup(:roguelixir_dungeon, :dungeon)[:dungeon]
+  end
+
+  defp load_yaml do
+    path = Path.join(File.cwd!(), "config/yml/dungeon.yml")
+    {:ok, yml_data} = YamlElixir.read_from_file(path)
+    yml_data
   end
 end
